4ec812a53d3ad9489d4386bd4737ee989fe50344
lottekoblens <lotte.koblens@hva.nl>
2022-01-07 20:42:52 +0100

    reverted test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header>
-      <h1>Morphological chart 1</h1>
+      <h1>Morphological chart</h1>
       </header>
       <Legend />
       <Map />

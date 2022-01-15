cc11f6aef06bee891c28b6fbbd0a90aef1f771a9
lottekoblens <lotte.koblens@hva.nl>
2022-01-15 21:10:18 +0100

    tried putting the tooltip in separate div, doesn't work yet

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,11 +2,7 @@ import React from "react";
 import data from "../data";
 
 function Tooltip() {
-  return (
-    <div>
-      <h3>{data.children.name}</h3>
-    </div>
-  );
+  return <div className="tooltipje"></div>;
 }
 
 export default Tooltip;
